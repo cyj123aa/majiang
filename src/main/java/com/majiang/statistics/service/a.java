0a0867085001e1a3e8ae6b691ff5ae9ef54fc4bf
陈岳军 <491199291@qq.com>
2019-09-09 11:26:05 +0800

    基础包上传  提交

---
+++ src/main/java/com/majiang/statistics/service/a.java
@@ -0,0 +1,10 @@
+package com.majiang.statistics.service;
+
+/**
+ * @author chenyuejun
+ * @descripption 为了目录结构
+ * @date 2019/9/9 &{TIME}
+ */
+public interface a {
+
+}
---
+++ src/main/java/com/majiang/statistics/service/impl/a.java
@@ -0,0 +1,10 @@
+package com.majiang.statistics.service.impl;
+
+/**
+ * @author chenyuejun
+ * @descripption  为了目录结构
+ * @date 2019/9/9 &{TIME}
+ */
+public class a {
+
+}
