63ef8e9c3e2748cc3560b4544cc670fa14eaafa0
陈岳军 <491199291@qq.com>
2019-10-17 17:18:43 +0800

    代码清洗

--- src/main/java/com/majiang/statistics/config/ShiroConfig.java
+++ src/main/java/com/majiang/statistics/config/ShiroConfig.java
@@ -1,20 +1,16 @@
 package com.majiang.statistics.config;
 
-import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
-import org.apache.shiro.mgt.DefaultSecurityManager;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * 描述：
